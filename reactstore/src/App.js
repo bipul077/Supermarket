0919bc0a57640a9ec55359f5e23d0bb259fc1736
sthaakin04 <akinstha97@gmail.com>
2022-12-18 17:35:48 +0545

    navbar added

--- reactstore/src/App.js
+++ reactstore/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Nav/Navbar';
 import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div>
-     <Footer/>
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
---
+++ reactstore/src/components/Nav/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Navbar.css"
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+    const menu=(e) => {
+        let menu = document.querySelector('#menu-icon');
+        let navbar = document.querySelector('.navbar');
+        
+        menu.onclick = () => {
+          menu.classList.toggle('bx-x');
+          navbar.classList.toggle('open');
+        }
+      }
+  return (
+    <div>
+      <header>
+            <a href='#' className='logo'><i class="ri-home-7-line"></i><span>Logo</span></a>
+            <ul className='navbar'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+
+            <div className='main'>
+                <div className='bx bx-menu' id="menu-icon" onClick={menu}></div>
+            </div>
+        </header>
+    </div>
+  )
+}
