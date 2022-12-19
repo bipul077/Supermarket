3a6f7c337cdca9e0e4ed2a57802822703cae8437
sthaakin04 <akinstha97@gmail.com>
2022-12-19 09:46:44 +0545

    home contents added

--- reactstore/src/App.js
+++ reactstore/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Nav/Navbar';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 function App() {
   return (
-    <div>
+    <div className="maincontent">
       <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   );
