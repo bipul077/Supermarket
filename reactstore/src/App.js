0fb622df07f2d3083df1704b3c499c8df4c4f4ae
bipul077 <bipulratna07@gmail.com>
2022-12-19 16:01:33 +0545

    contact us page added

--- reactstore/src/App.js
+++ reactstore/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Nav/Navbar';
 import Footer from './components/Footer/Footer';
 import Products from './components/Products/Products';
 import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
 import{
   BrowserRouter as Router,
   Routes,
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/products" element={<Products/>}/>
+          <Route exact path="/contact" element={<Contact/>}/>
         </Routes>
         <Footer/>
       </Router>   
