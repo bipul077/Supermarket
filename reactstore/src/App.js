15c09de0da384ec74a81e7763cc966cb819d337b
sthaakin04 <akinstha97@gmail.com>
2022-12-19 15:30:26 +0545

    about us

--- reactstore/src/App.js
+++ reactstore/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Nav/Navbar';
 import Footer from './components/Footer/Footer';
 import Products from './components/Products/Products';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 import{
   BrowserRouter as Router,
   Routes,
@@ -15,6 +16,7 @@ function App() {
       <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
           <Route exact path="/products" element={<Products/>}/>
         </Routes>
         <Footer/>
